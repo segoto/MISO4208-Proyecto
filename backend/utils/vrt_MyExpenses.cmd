4b9542fb3bee14fb3203470e12b896f03360729d
Mateo Salcedo <m.salcedo@uniandes.edu.co>
2020-11-14 00:26:06 -0500

    variable folder

--- backend/utils/vrt_MyExpenses.cmd
+++ backend/utils/vrt_MyExpenses.cmd
@@ -1,6 +1,7 @@
 @echo off
 
 echo Start
+set folder=C:\Users\teo-1\Desktop\MISO4208-Proyecto\backend\utils\mod_images
 cd C:\Users\teo-1\AppData\Local\Android\Sdk\platform-tools
 adb shell input swipe 535 1840 535 840 100
 adb shell input swipe 535 1840 535 1440 100
@@ -8,12 +9,12 @@ adb shell input tap 670 1510
 adb shell input tap 842 1764
 ping -n 5 127.0.0.1 >nul
 adb shell screencap /sdcard/screen.png
-adb pull /sdcard/screen.png C:\Users\teo-1\Desktop\MISO4208-Proyecto\backend\utils\mod_images\myexpenses_img1.png
+adb pull /sdcard/screen.png %folder%\myexpenses_img1.png
 
 adb shell input tap 70 150
 ping -n 2 127.0.0.1 >nul
 adb shell screencap /sdcard/screen.png
-adb pull /sdcard/screen.png C:\Users\teo-1\Desktop\MISO4208-Proyecto\backend\utils\mod_images\myexpenses_img2.png
+adb pull /sdcard/screen.png %folder%\myexpenses_img2.png
 
 adb shell input tap 962 2103
 ping -n 1 127.0.0.1 >nul
@@ -22,13 +23,13 @@ ping -n 1 127.0.0.1 >nul
 adb shell input keyevent 4
 ping -n 1 127.0.0.1 >nul
 adb shell screencap /sdcard/screen.png
-adb pull /sdcard/screen.png C:\Users\teo-1\Desktop\MISO4208-Proyecto\backend\utils\mod_images\myexpenses_img3.png
+adb pull /sdcard/screen.png %folder%\myexpenses_img3.png
 
 adb shell input tap 70 150
 adb shell input tap 890 140
 ping -n 2 127.0.0.1 >nul
 adb shell screencap /sdcard/screen.png
-adb pull /sdcard/screen.png C:\Users\teo-1\Desktop\MISO4208-Proyecto\backend\utils\mod_images\myexpenses_img4.png
+adb pull /sdcard/screen.png %folder%\myexpenses_img4.png
 
 echo End
 
